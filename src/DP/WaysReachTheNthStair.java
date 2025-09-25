7e8b138b527ffef9ae1342df6d458c4279ffab0b
Sonu Maddheshiya <96307808+sonu-maddy@users.noreply.github.com>
2025-09-25 18:02:32 +0530

    updated

--- src/DP/WaysReachTheNthStair.java
+++ src/DP/WaysReachTheNthStair.java
@@ -1,7 +1,48 @@
 package DP;
 
 public class WaysReachTheNthStair {
+    int countWays(int n) {
+        // 1st ways
+        // if(n == 1) return 1;
+        // int[] dp = new int[n+1];
 
+        // dp[1] = 1;
+        // dp[2] = 2;
+
+        // for(int i = 3; i<=n; i++){
+        //     dp[i] = dp[i-1] + dp[i-2];
+        // }
+
+        // return dp[n];
+
+        // 2nd ways
+        // int ans = 0;
+        // if(n==0) return 0;
+        // if(n ==1) return 1;
+        // if(n == 2) return 2;
+
+        // ans = countWays(n-1) + countWays(n-2);
+
+        // return ans;
+
+        //3rd ways
+
+        if(n <= 2) return n;
+        int first =1;
+        int second =2;
+        int result =0;
+
+        for(int i =3; i<=n; i++){
+            result = first + second;
+            first = second;
+            second = result;
+        }
+
+        return result;
+
+
+
+    }
 }
 
 //There are n stairs, a person standing at the bottom wants to reach the top. The person can climb either 1 stair or 2 stairs at a time. Your task is to count the number of ways, the person can reach the top (order does matter).
